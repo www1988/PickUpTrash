58160e9360655699c03755681dbc29a0e957e9e1
刘万 <206046160@qq.com>
2021-01-26 10:48:27 +0800

    - 更新

--- main.py
+++ main.py
@@ -3,10 +3,17 @@
 # @Author : huruwo
 # @File : main
 # @Function : 后台主页
+import queue
+
 from sanic import Sanic
 from sanic.response import text
 
+from data_save import MySqlUtils
+
 app = Sanic(name='main_app')
+sql_util = MySqlUtils()
+
+task_queue = queue.Queue(maxsize=400000)
 
 
 @app.route('/goods', methods=['POST'])
@@ -16,8 +23,37 @@ async def index(request):
     :param request:
     :return:
     """
-    print(request.json)
+    data = request.json
+    # name,price,address,describe,other
+    name = data.get('name')
+    price = data.get('price')
+    address = data.get('address')
+    describe = data.get('describe')
+    other = data.get('other')
+    sql_util.insert_data(name, price, address, describe, other)
     return text('提交成功')
+
+
+@app.route('/task', methods=['POST'])
+async def index(request):
+    """
+    提交任务
+    :param request:
+    :return:
+    """
+    data = request.json
+    task_queue.put(data.get('task'))
+    return text('提交成功')
+
+
+@app.route('/task', methods=['GET'])
+async def index(request):
+    """
+    提取任务
+    :param request:
+    :return:
+    """
+    return task_queue.get()
 
 
 if __name__ == '__main__':
